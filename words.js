3277ea8d391fb7ad5c23d6777e84169469065aeb
PJSalter <psalter88@googlemail.com>
2022-02-01 22:12:26 +0000

    function in creating the game board, initiating the letter in squares

---
+++ words.js
@@ -0,0 +1,27 @@
+
+let heightInner = 6; // this will initiate the number of tries the user has.
+let lengthOfWord = 5; //this will initiate the length of the word, the user can only spell out 5 letters each word.
+
+// JavaScript to create the game board
+
+//allows the function to load
+
+window.onload = function(){
+    initialize();
+}
+
+function initialize(){
+
+    //making the board
+    for (let i = 0; i < heightInner; i++){
+        for (let j = 0; j < lengthOfWord; j++){
+            // example - <span id="0-0" class="tile">p</span>
+            let squareTile = document.createElement("span");
+            squareTile.id = i.toString() + "-" + j.toString();
+            squareTile.classList.add("square")
+            // placeholder letter p
+            squareTile.innerText = "p";
+            document.getElementById("word-grid").appendChild(squareTile);
+        }
+    }
+}
